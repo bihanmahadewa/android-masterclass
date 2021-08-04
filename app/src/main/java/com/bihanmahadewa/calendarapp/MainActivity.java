8fa8f1df1d67393d6d0e8854d0cdb6ada34965b3
instructor <bihanscourses@gmail.com>
2021-08-04 15:19:51 +0530

    first commit

---
+++ app/src/main/java/com/bihanmahadewa/calendarapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.bihanmahadewa.calendarapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CalendarView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CalendarView calendarView = findViewById(R.id.calendar_view);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+
+            }
+        });
+    }
+}
