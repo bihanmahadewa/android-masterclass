6d2ee464383647ffcb2a73ec87ee31d7dc57ac80
instructor <bihanscourses@gmail.com>
2021-08-04 15:33:19 +0530

    first commit

---
+++ app/src/main/java/com/bihanmahadewa/webview/MainActivity.java
@@ -0,0 +1,20 @@
+package com.bihanmahadewa.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.web_view);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://udemy.com");
+    }
+}
